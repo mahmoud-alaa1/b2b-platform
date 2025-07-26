ef572a785c2cb5e389c3102612e175c161bc309d
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-07-26 18:04:17 +0300

    feat: update dependencies and refactor project structure
    - Added new dependencies: @radix-ui/react-slot, @tanstack/react-query, axios, class-variance-authority, clsx, lucide-react, tailwind-merge, zustand, tw-animate-css.
    - Removed globals.css and migrated styles to src/styles/globals.css.
    - Created Providers and ReactQueryProvider components for context management.
    - Implemented a custom Button component with variants using class-variance-authority.
    - Added useLogin hook and useStore with Zustand for state management.
    - Set up Axios instance with interceptors for API requests.
    - Updated layout to include Providers for context wrapping.
    - Cleaned up unused files and organized project structure.

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+}
+
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
+}
