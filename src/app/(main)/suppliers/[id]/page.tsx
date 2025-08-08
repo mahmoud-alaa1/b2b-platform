cc7312a9c1954b284d26398eb4296c72f09e3d7f
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-08-08 16:53:09 +0300

    feat: add account settings and supplier pages with basic structure

---
+++ src/app/(main)/suppliers/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default async function page({ params }: {
+    params: Promise<{
+        id: string
+    }>
+}) {
+    const id = (await params).id;
+    return (
+        <div>page {id}</div>
+    )
+}
